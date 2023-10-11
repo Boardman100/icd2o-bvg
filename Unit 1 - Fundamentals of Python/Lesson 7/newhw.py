80dd0159f4e45052b0759c894d610b738509ebea
Stefano Edwards <sedwards@bayviewglen.ca>
2023-10-10 22:18:02 -0400

    update folder names, fix weird folder

---
+++ Unit 1 - Fundamentals of Python/Lesson 7/newhw.py
@@ -0,0 +1,16 @@
+city = "City"
+population = "Population"
+area = "Area (sq km)"
+city1 = "New York"
+city2 = "Los Angeles"
+city3 = "Chicago"
+num1 = "2,239,000"
+num2 = "3,239,000"
+num3 = "4,239,000"
+num4 = "339,000"
+num5 = "239,000"
+num6 = "639,000"
+print(f"{city:<14}{population:<15}{area}")
+print(f"{city1:<14}{num1:<15}{num4}")
+print(f"{city2:<14}{num2:<15}{num5}")
+print(f"{city3:<14}{num3:<15}{num6}")
---
+++ Unit 1 - Fundamentals of Python/Lesson 7/test.py
@@ -0,0 +1,10 @@
+title1 = "Item"
+title2 = "Price"
+title3 = "Quantity"
+title4 = "Total"
+item = "Product"
+price = 25.99
+quantity = 3
+total = price*quantity
+print(f"{title1:<9}{title2:<9}{title3:<11}{title4:<8}")
+print(f"{item:<9}{price:<16}{quantity:<4}{total}")
